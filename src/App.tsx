cd2426230144030982df18bbc23c930760367bbe
Cristopher Cunas <cristophercunas@icloud.com>
2022-07-13 23:31:18 -0400

    added in async handler to grab selected symbols then resetting selected creators. Added in ternary to handle loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Data from './data/sp500.json';
+import { useState } from 'react';
 import { HeroSection } from './components/HeroSection';
 import { DataTable } from './partials/tables/DataTable';
 var _ = require('lodash')
@@ -6,10 +7,20 @@ var _ = require('lodash')
 
 function App() {
 
+    const [results, setResults] = useState<any>([])
+
+    const generateResults = async (selected: object):Promise<any> => {
+        console.log(selected)
+
+    }
+
     return (
         <div>
             <HeroSection />
-            <DataTable dataSet={Data} />
+            <DataTable
+                dataSet={Data}
+                handler={generateResults}
+            />
         </div>
     );
 }
