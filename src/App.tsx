48bf16c9bdf3a98ae90554b5b3bcc462e370757a
Cristopher Cunas <cristophercunas@icloud.com>
2022-07-15 01:57:22 -0400

    set up axios instance in seperate service folder, env not working for some reason

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import configs from '../src/service/configs';
 import Data from './data/sp500.json';
 import { useState } from 'react';
 import { HeroSection } from './components/HeroSection';
@@ -7,11 +8,16 @@ var _ = require('lodash')
 
 function App() {
 
-    const [results, setResults] = useState<any>([])
+    const [searchResults, setSearchResults] = useState<any>([])
 
-    const generateResults = async (selected: object):Promise<any> => {
-        console.log(selected)
+    const generateResults = async (selected: {name: string, id: number}[]):Promise<void> => {
+        let symbol = selected[0].name
 
+        configs.get(`aggs/ticker/${symbol}/prev?adjusted=true`).then(({data})=> {
+            console.log(data.results)
+        }).catch((error) => {
+            console.log(error)
+        })
     }
 
     return (
